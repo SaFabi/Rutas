cd24c803ce74a37fac28fafc9ebc693a6f993f66
SaFabi <ti13110050@itess.edu.mx>
2018-01-15 17:55:58 -0600

    Se agrego el fragmento para ver los detalles de los creditos

---
+++ app/src/main/java/com/example/fabi/atc/Adapters/AdapterDetallesCreditos.java
@@ -0,0 +1,97 @@
+package com.example.fabi.atc.Adapters;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.textclassifier.TextClassification;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.fabi.atc.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+public class AdapterDetallesCreditos  extends BaseAdapter{
+    private JSONArray array;
+    private Context context;
+    TextView tipo,marca,modelo,cantidad,monto;
+
+
+    public AdapterDetallesCreditos(JSONArray array, Context context) {
+        this.array = array;
+        this.context = context;
+    }
+
+
+    @Override
+    public int getCount() {
+        return array.length();
+    }
+
+    @Override
+    public JSONObject getItem(int i) {
+        JSONObject jsonObject;
+
+        try
+        {
+            jsonObject = array.getJSONObject(i);
+        }
+        catch (JSONException e)
+        {
+            jsonObject = null;
+        }
+
+        return jsonObject;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View view = convertView;
+        if (convertView == null){
+            view = inflater.inflate(R.layout.modelodetallescreditos,null);
+        }
+        tipo = (TextView)view.findViewById(R.id.tipoArticulo);
+        marca = (TextView)view.findViewById(R.id.marca);
+        modelo = (TextView)view.findViewById(R.id.modelo);
+        cantidad = (TextView)view.findViewById(R.id.Cantidad);
+        monto = (TextView)view.findViewById(R.id.monto);
+
+        String Stipo, Smarca, Smodelo, Scantidad, Smonto;
+        try {
+
+            Stipo = getItem(i).getString("0");
+            Smarca = getItem(i).getString("1");
+            Smodelo = getItem(i).getString("2");
+            Scantidad = getItem(i).getString("3");
+            Smonto = getItem(i).getString("4");
+        }catch (JSONException e){
+            Stipo = null;
+            Smarca = null;
+            Smodelo = null;
+            Scantidad = null;
+            Smonto = null;
+        }
+            if (Stipo != null) {
+            tipo.setText(Stipo);
+            marca.setText(Smarca);
+            modelo.setText(Smodelo);
+            cantidad.setText("Cantidad: "+Scantidad);
+            monto.setText("Monto total: "+Smonto);
+
+            }
+        return view;
+    }
+}
--- app/src/main/java/com/example/fabi/atc/Fragmentos/detallesCreditosPendientes.java
+++ app/src/main/java/com/example/fabi/atc/Fragmentos/detallesCreditosPendientes.java
@@ -1,20 +1,38 @@
 package com.example.fabi.atc.Fragmentos;
 
+import android.app.ProgressDialog;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.fabi.atc.Adapters.AdapterClientes;
+import com.example.fabi.atc.Adapters.AdapterDetallesCreditos;
+import com.example.fabi.atc.Clases.Basic;
+import com.example.fabi.atc.Clases.ModeloClientes;
 import com.example.fabi.atc.R;
 
+import org.json.JSONArray;
 
-public class detallesCreditosPendientes extends Fragment {
+
+public class detallesCreditosPendientes extends Fragment  implements Basic{
 
     private OnFragmentInteractionListener mListener;
     int ordenID;
+    ProgressDialog progressDialog;
+    AdapterDetallesCreditos adapter;
+    ListView listView;
 
     public detallesCreditosPendientes() {
         // Required empty public constructor
@@ -42,6 +60,45 @@ public class detallesCreditosPendientes extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View vista =  inflater.inflate(R.layout.fragment_detalles_creditos_pendientes, container, false);
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setTitle("En Proceso");
+        progressDialog.setMessage("Un momento...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.show();
+        listView = (ListView)vista.findViewById(R.id.detallesCreditos);
+
+        //CONSULTA PATA OBTENER TODOS LOS CREDITOS REGISTRADOS DE UN CLIENTE EN ESPECIFICO
+        RequestQueue queueCreditos = Volley.newRequestQueue(getContext());
+        String consultaCreditos = "select distinct ta.nombre,ma.nombre, mo.nombre, orddesc.cantidad,(orddesc.precio_final * orddesc.cantidad) " +
+                "from tipo_articulo ta, modelo mo, marca ma, articulo a, orden_descripcion orddesc, orden ord, cantidad ca " +
+                "where ta.id = a.tipoArticulo_id " +
+                "and a.modelo_id = mo.id " +
+                "and mo.marca_id = ma.id " +
+                "and ca.id = orddesc.tipoVentaId " +
+                "and ca.articulo_id = a.id " +
+                "and orddesc.orden_id = ord.id " +
+                "and ord.id="+ordenID;
+        consultaCreditos = consultaCreditos.replace(" ", "%20");
+        String cadenaCreditos = "?host=" + HOST + "&db=" + DB + "&usuario=" + USER + "&pass=" + PASS + "&consulta=" + consultaCreditos;
+        String url = SERVER + RUTA + "consultaGeneral.php" + cadenaCreditos;
+        Log.i("info", url);
+        JsonArrayRequest requestCreditos = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                progressDialog.hide();
+               adapter = new AdapterDetallesCreditos(response,getContext());
+                listView.setAdapter(adapter);
+
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queueCreditos.add(requestCreditos);
         Toast.makeText(getContext(),String.valueOf(ordenID),Toast.LENGTH_SHORT).show();
 
 
